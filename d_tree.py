2b14dddef7b3d14740739ead756ed481f5ffec3d
Jackson Watkins <jacksonwatkins@Jacksons-MacBook-Pro-2.local>
2018-12-14 15:48:54 -0800

    removed the skipline

--- d_tree.py
+++ d_tree.py
@@ -7,7 +7,7 @@ COLUMNS = ["age", "work class", "education", "education-num", "marital_status", 
 
 # reading training and test data
 df_train = pd.read_csv('income-training.csv', names=COLUMNS)
-df_test = pd.read_csv('income-test.csv', names=COLUMNS, skiprows=1)
+df_test = pd.read_csv('income-test.csv', names=COLUMNS)
 
 # f illing Nan's with -999999
 df_train.fillna(-99999)
@@ -15,6 +15,8 @@ df_test.fillna(-99999)
 
 le = preprocessing.LabelEncoder()
 
+
+#fit and transform
 for x in COLUMNS:
     if df_train[x].dtypes == 'object':
         data = df_train[x].append(df_test[x])
@@ -22,5 +24,6 @@ for x in COLUMNS:
         df_train[x] = le.transform(df_train[x])
         df_test[x] = le.transform(df_test[x])
 
-
+print(df_train.head())
+print(df_test.head())
 # X = [[25, Private, 11th, 7, Never-married, Machine-op-inspect, Own-child, Black, Male, 0, 0, 40, United-States, <=50k]]
