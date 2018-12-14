8268ba6956c58f41d883ede1de24c50047495b73
basillatif <basillatif@gmail.com>
2018-12-13 17:49:50 -0800

    CHANGED THE EXTENSION NAME FROM '.TXT' TO '.CSV'

---
+++ naive_bayes.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+from sklearn import linear_model, preprocessing, tree
+
+
+COLUMNS = ["age", "work class", "education", "education-num", "marital_status", "occ_code", "relationship", "race", "sex", "cap_gain", "cap_loss", "hours_per_week", "native_country"]
+
+#reading training and test data
+TRAININGSET = pd.read_csv('income.training.csv')
+TESTSET = pd.read_csv('income-test.csv')
+test_data = open
+
+#filling Nan's with -999999
+fillNans_training_data.fillna(-99999)
+fillNans_in_te.fillna(-99999)
