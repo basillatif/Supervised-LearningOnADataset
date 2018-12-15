74932844ce73ba907cf3a0450bf53d5b5d9c7004
Jackson Watkins <jacksonwatkins@Jacksons-MacBook-Pro-2.local>
2018-12-14 18:44:16 -0800

    Added some comments related to how we should preprocess each column

--- d_tree.py
+++ d_tree.py
@@ -1,10 +1,10 @@
 import numpy as np
 import pandas as pd
 from sklearn import linear_model, preprocessing, tree
-from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder
+from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder, Normalizer, KBinsDiscretizer
+from sklearn.compose import ColumnTransformer
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import cross_val_score
-
 
 COLUMNS = ["age", "work class", "education", "education-num", "marital_status", "occ_code", "relationship", "race", "sex", "cap_gain", "cap_loss", "hours_per_week", "native_country", "income"]  # noqa: E501
 
@@ -21,7 +21,6 @@ ohe = OneHotEncoder(sparse=False)
 ordinal = OrdinalEncoder()
 encoder = le
 
-
 # fit and transform
 for x in COLUMNS:
     if df_train[x].dtypes == 'object':
@@ -31,6 +30,10 @@ for x in COLUMNS:
         df_test[x] = encoder.fit_transform(df_test_2d)
 
 print(df_train)
+
+# ct = ColumnTransformer([("norm1", KBinsDiscretizer(n_bins=5, encode=’ordinal’, strategy=’quantile’), [0, 1]), ("norm2", Normalizer(norm='l1'), slice(2, 4))])
+
+
 # clf = DecisionTreeClassifier(random_state=0)
 # score = cross_val_score(clf, df_train, COLUMNS)
 # X = [[25, Private, 11th, 7, Never-married, Machine-op-inspect, Own-child, Black, Male, 0, 0, 40, United-States, <=50k]]
